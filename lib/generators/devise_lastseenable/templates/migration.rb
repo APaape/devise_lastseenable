285f9c2ab228f40c107f965a769dc35fa50591d8
APaape <github.com@apaape.de>
2021-06-04 17:48:19 +0200

    Use #update_column instead of #save

--- lib/generators/devise_lastseenable/templates/migration.rb
+++ lib/generators/devise_lastseenable/templates/migration.rb
@@ -1,9 +1,9 @@
 class DeviseAddLastseenable<%= table_name.camelize.singularize %> < ActiveRecord::Migration
   def self.up
-    add_column :<%= table_name %>, :last_seen, :datetime
+    add_column :<%= table_name %>, :last_seen_at, :datetime
   end
-  
+
   def self.down
-    remove_column :<%= table_name %>, :last_seen
+    remove_column :<%= table_name %>, :last_seen_at
   end
 end
